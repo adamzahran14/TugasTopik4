070428a017cc73f1c86af65a3c42b9bca3d7a955
adamzahran14 <adamzahran72@gmail.com>
2021-10-20 22:25:54 +0700

    src

---
+++ src/com/tugastopik4/Buku.java
@@ -0,0 +1,37 @@
+package com.tugastopik4;
+
+public class Buku {
+    private String judul;
+    private String pengarang;
+    private double harga;
+
+    public Buku(String judul, String pengarang, double harga) {
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.harga = harga;
+    }
+
+    public void setJudul() {
+        this.judul = judul;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setPengarang() {
+        this.pengarang = pengarang;
+    }
+
+    public String getPengarang() {
+        return pengarang;
+    }
+
+    public void setHarga() {
+        this.harga = harga;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+}
---
+++ src/com/tugastopik4/Main.java
@@ -0,0 +1,13 @@
+package com.tugastopik4;
+
+public class Main {
+    public static void main (String[] args ) {
+        Buku buku1 = new Buku("Harry Potter", "J.K. Rowling", 300000);
+        Buku buku2 = new Buku("UML", "Ivar Jacobson", 400000);
+
+        System.out.println("Data Buku : ");
+        System.out.println(buku1.getJudul() + " - " + buku1.getPengarang() + " - " + buku1.getHarga());
+        System.out.println(buku2.getJudul() + " - " + buku2.getPengarang() + " - " + buku2.getHarga());
+
+    }
+}
